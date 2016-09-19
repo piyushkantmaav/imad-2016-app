748cd90b9c112e87d4fb8f80ca34e625e421975c
piyushkantmaav <kantmaav@gmail.com>
2016-09-20 00:47:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,11 +13,13 @@ app.get('/article-one',function(req, res){
 }
 );
 app.get('/article-two',function(req, res){
-    res.send('Article-two is requested and will be server here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+
 }
 );
 app.get('/article-three',function(req, res){
-    res.send('Article-three is requested and will be server here');
+      res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+
 }
 );
 
