40c00f2bc6a098f19fa328bb48d205f85b5e7b70
piyushkantmaav <kantmaav@gmail.com>
2016-09-21 22:45:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ var img = document.getElementById('piya');
 var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+5;
-    img.style.marginLeft=margin+'px';
+    img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
     var interval=setInterval(moveRight,50);
