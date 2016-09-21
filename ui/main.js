78947b106aa220a32b9682a2c23015a2e846822f
piyushkantmaav <kantmaav@gmail.com>
2016-09-21 22:17:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //change the text of main text div
 var element = document.getElementById('main-text');
 element.innerHTML="New value";
+//move img
+var img = document.getElementById('piya');
+img.onclick=function(){
+    img.style.marginleft='100px';
+    
+};
