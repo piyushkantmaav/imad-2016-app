e1ea84685ce771b903f797122282bb08823f2b41
piyushkantmaav <kantmaav@gmail.com>
2016-09-24 00:22:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,8 @@
-console.log('Loaded!');
-//change the text of main text div
-var element = document.getElementById('main-text');
-element.innerHTML="New value";
-//move img
-var img = document.getElementById('piya');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    //render the variable in correct spam
+  counter = counter +1  ;
+  var spa  = document.getElementById('count');
+  span.innerHTML=counter.toString();
 };
