673b024b35b6d697cb7d42c11f350dd522b15581
piyushkantmaav <kantmaav@gmail.com>
2016-09-26 00:06:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,14 +16,14 @@ button.onclick = function(){
  
 };
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function (){
-    //make a request to the and send the name
-    
-    //capture a list of name and render it as a list
-    var names =['name1','name2','name3','name4'];
+      var request= new XMLHttpRequest();
+    request.onreadystatechange = function(){
+   if (request.readyState ===XMLHttpRequest.DONE ){
+       if(request.status===200){
+            var names = request.responseText;
+            names = JSON.parse(names);
     var list = '';
     for (var i=0;i<names.length;i++){
         list +='<li>'+names[i]+'</li>';
@@ -31,4 +31,18 @@ submit.onclick = function (){
     }
 var ul = document.getElementById('namelist');
 ul.innerHTML = list;
+        
+       }
+   }     
+  };
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+ request.open('GET','http://piyushkantmaav.imad.hasura-app.io/submit-name?name='+name,true);
+ request.send(null);
+
+    //make a request to the and send the name
+    
+    //capture a list of name and render it as a list
+ 
 };
