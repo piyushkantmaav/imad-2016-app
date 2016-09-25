f2cb1a94e2410bff71e84790b8a87ac986503401
piyushkantmaav <kantmaav@gmail.com>
2016-09-25 17:57:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,6 +101,13 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  
+});
+var names=[];
+app.get('/submit-name/:name',function(req,res){
+    var name = req.params.name;
+    name.push(name);
+    res.send(JSON.stringify(names));
 });
 
 
